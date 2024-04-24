b976865089ac29d5c69fcfd4a519c0a432d98748
Ciro Mora <ciro.mora@gmail.com>
2024-04-24 17:49:42 +0200

    Initial commit

---
+++ src/main/java/com/artisan/infraconfig/Application.java
@@ -0,0 +1,13 @@
+package com.artisan.infraconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
